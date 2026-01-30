f33a9d7fe2f0eeb10df26e5a113ec2e9369b7482
jaysonreyes001 <jaysonreyes012345@gmail.com>
2026-01-30 11:27:41 +0800

    upload

--- src/Pages/projectView.vue
+++ src/Pages/projectView.vue
@@ -1,6 +1,6 @@
 <template lang="">
      <section class="min-h-[100dvh] mt-[180px] md:mt-[120px] app-size">
-          <div class="flex items-center">
+          <div class="md:flex items-center space-y-5 md:space-y-0">
                <button @click="router.back()" class="hover:shadow transition border dark:border-primary-dark/30 dark:border-primary-light/30 shadow-md 
                dark:shadow-primary-dark/30 shadow-primary-light/30 rounded-md px-3 py-2 md:text-md text-xs flex items-center gap-x-1  rounded">
                     <v-icon class="md:text-2xl text-lg mr-1" icon="material-symbols:arrow-left-alt"/>
@@ -8,11 +8,11 @@
                </button>
                
 
-               <nav class="flex md:ml-10 ml-7" aria-label="Breadcrumb">
+               <nav class="flex md:ml-10" aria-label="Breadcrumb">
                     <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                          <li class="text-gray-600 dark:text-gray-200">
                               <div class="flex items-center space-x-1.5">
-                              <a href="#" class="text-sm md:text-lg inline-flex items-center font-medium text-body hover:text-fg-brand">Projects</a>
+                              <a href="#" class="text-sm md:text-md inline-flex items-center font-medium text-body hover:text-fg-brand">Projects</a>
                               </div>
                          </li>
                          <li aria-current="page">
